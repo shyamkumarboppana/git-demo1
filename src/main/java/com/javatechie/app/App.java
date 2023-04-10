18ad36d15d4d0b37993b28cf9863437e03a74e33
shyamkumar <shyam.bopanna83@gmail.com>
2023-04-10 20:16:02 +0530

    adding git-demo1 project to localrepository

---
+++ src/main/java/com/javatechie/app/App.java
@@ -0,0 +1,13 @@
+package com.javatechie.app;
+
+public class App {
+	
+	public void addData(String name) {
+		System.out.println("name: " + name);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
