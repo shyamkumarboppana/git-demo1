1859e7c5e9af1f7c4eee3c88dca34c9e2b8cb6cb
shyamkumar <shyam.bopanna83@gmail.com>
2023-04-10 20:25:03 +0530

    adding modified files to local repo

--- src/main/java/com/javatechie/app/App.java
+++ src/main/java/com/javatechie/app/App.java
@@ -4,6 +4,9 @@ public class App {
 	
 	public void addData(String name) {
 		System.out.println("name: " + name);
+		if (name.equals("shyam")) {
+			System.out.println("welcome");
+		}
 	}
 	
 	public static void main(String[] args) {
